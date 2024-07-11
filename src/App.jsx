7bc44528f01fd9fff708314264664dd32a1a9afa
dhruvathakur <128789248+DhruvaPratapSingh@users.noreply.github.com>
2024-07-11 12:32:05 +0530

    ready for editing adding new work

--- src/App.jsx
+++ src/App.jsx
@@ -40,10 +40,6 @@ function App() {
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos))
   }, [todos])
-  
-
-
-
   return (
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
       <div className="bg-[#172842] min-h-screen py-8">
